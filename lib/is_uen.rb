e1728724fc9055709098b6d22d2bbd540c39e193
yaojie <limyaojie93@gmail.com>
2022-01-24 21:34:36 +0800

    Add valid_year? validation

--- lib/is_uen.rb
+++ lib/is_uen.rb
@@ -7,7 +7,8 @@ module IsUen
   VALID_ENTITIES = %w[lp ll fc pf rf mq mm nb cc cs mb fm gs dp cp nr cm cd md hs vh ch mh cl xl cx hc rp tu tc fb fn
                       pa pb ss mc sm ga gb].freeze
 
-  ENTITY_REGEX = /^(?<year>r|s|t)\d{2}(?<entity>[a-z]{2})\d{4}[a-z]$/.freeze
+  YEARS = { "r" => "18", "s" => "19", "t" => "20" }.freeze
+  ENTITY_REGEX = /^(?<year>r|s|t)(?<decade>\d{2})(?<entity>[a-z]{2})\d{4}[a-z]$/.freeze
 
   def uen?(id)
     business?(id) || company?(id) || new_uen?(id)
@@ -24,17 +25,27 @@ module IsUen
   end
 
   def company?(id)
-    /^\d{9}[a-z]$/i.match?(id)
+    /^(?<year>\d{4})\d{5}[a-z]$/i.match(id) do |match|
+      valid_year?(match[:year])
+    end
   end
 
   # FIXME: not sure how to effectively test these! will revisit
   VALID_ENTITIES.each do |entity|
     define_singleton_method "#{entity}?" do |id|
       ENTITY_REGEX.match(id.downcase) do |match|
-        entity == match[:entity]
+        year = YEARS[match[:year]] + match[:decade]
+
+        entity == match[:entity] && valid_year?(year)
       end
     end
   end
+
+  def self.valid_year?(year_string)
+    year_string.to_i <= Time.now.year
+  end
+
+  private_class_method :valid_year?
 
   module_function :uen?, :business?, :company?, :new_uen?
 end
